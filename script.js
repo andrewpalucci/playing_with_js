2efb0eb154e0c44a9db2b0f4a1cb40b02f7e02ce
andrewpalucci <palucci.andrew@gmail.com>
2025-05-21 16:04:36 -0400

    Rock Paper Scissors Game Coded
    All details in script.js file. Can now play 5 rounds of "rock, paper, scissors" with the computer!

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const prompt = require("prompt-sync")({sigint:true});
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     //Get the computer to return a random integer from 0 to 2
     let compChoice;
     compChoice = Math.floor(Math.random() * 3);
-    console.log(compChoice)
 
     switch (compChoice) {
         case 0:
@@ -35,4 +37,69 @@ function getHumanChoice() {
     return userChoice
 }
 
-getHumanChoice()
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "paper":
+            if (computerChoice === "scissors") {
+                console.log("You loose! The computer chose \"scissors\".")
+                computerScore++
+            }
+            else {
+                console.log("You win! The computer chose " + computerChoice + ".")
+                humanScore++
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                console.log("You loose! The computer chose \"rock\".")
+                computerScore++
+            }
+            else {
+                console.log("You win! The computer chose " + computerChoice + ".")
+                humanScore++
+            }
+            break;
+        case "rock":
+            if (computerChoice === "paper") {
+                console.log("You loose! The computer chose \"paper\".")
+                computerScore++
+            }
+            else {
+                console.log("You win! The computer chose " + computerChoice + ".")
+                humanScore++
+            }
+    }
+}
+
+function printScore() {
+    console.log("\nThe score is: \nComputer: " + computerScore + "\nYou: " + humanScore + "\n")
+}
+
+function playGame() {
+    console.log("Welcome to rock paper scissors!")
+
+    for (let round = 1; round <= 5; round ++) {
+
+        console.log("Round " + round + "!")
+
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
+        humanChoice = humanChoice.toLowerCase()
+        computerChoice = computerChoice.toLowerCase()
+
+        playRound(humanChoice, computerChoice)
+
+        printScore()
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congrats! You won! Re-run this file to play again.")
+    }
+    else {
+        console.log("Sorry! You lost. Rerun this file to try again.")
+    }
+
+}
+
+
+playGame()
